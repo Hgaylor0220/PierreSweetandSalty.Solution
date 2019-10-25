d3db2b5119edb6ec52f4b4af06ee31432c006d8b
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-25 16:27:47 -0700

    completed edit function in controller for treat and views

--- PierreSweetandSalty/Controllers/TreatsController.cs
+++ PierreSweetandSalty/Controllers/TreatsController.cs
@@ -23,5 +23,20 @@ namespace PierreSweetandSalty.Controllers
             _userManager = userManager;
             _db = db;
         }
+
+        public async Task<ActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id);
+            return View(userTreats);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorCategory");
+            return View();
+        }
+
     }
 }
