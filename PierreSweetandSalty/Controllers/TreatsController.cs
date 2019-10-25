327a6b2e566e4b31517ac00318336e44259c8c96
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-25 16:43:30 -0700

    completed the add flavor controller and view

--- PierreSweetandSalty/Controllers/TreatsController.cs
+++ PierreSweetandSalty/Controllers/TreatsController.cs
@@ -34,7 +34,7 @@ namespace PierreSweetandSalty.Controllers
 
         public ActionResult Create()
         {
-            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorCategory");
+            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
             return View();
         }
 
@@ -52,6 +52,60 @@ namespace PierreSweetandSalty.Controllers
             
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var thisTreat = _db.Treats
+                .Include(treat => treat.Flavors)
+                .ThenInclude(join => join.Flavor)
+                .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+    
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat, int FlavorId, int IngredientId)
+        {
+            if (FlavorId != 0)
+            {
+                _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+            }
+            
+            _db.Entry(treat).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddFlavor(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+            ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult AddFlavor(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0)
+            {
+                _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddIngredient(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+            return View(thisTreat);
         }
 
     }
