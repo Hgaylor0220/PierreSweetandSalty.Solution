993494d884bc0638085f4ce57fff53995bcdac5e
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-25 12:39:56 -0700

    added controller function for user

--- PierreSweetandSalty/Controllers/FlavorsController.cs
+++ PierreSweetandSalty/Controllers/FlavorsController.cs
@@ -5,11 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PierreSweetandSalty.Models;
+using PierreSweetandSalty.Data;
 
 namespace PierreSweetandSalty.Controllers
 {
     public class FlavorsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public FlavorsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
     }
 }
--- PierreSweetandSalty/Controllers/TreatsController.cs
+++ PierreSweetandSalty/Controllers/TreatsController.cs
@@ -1,15 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PierreSweetandSalty.Data;
 using PierreSweetandSalty.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierreSweetandSalty.Controllers
 {
+    [Authorize]
     public class TreatsController : Controller
     {
-        
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager <ApplicationUser> _userManager;
+
+        public TreatsController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
     }
 }
