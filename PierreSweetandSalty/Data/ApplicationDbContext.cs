463165d2b0c13eb14ee2a61c1de3735debcc17b6
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-25 11:34:13 -0700

    added properties in models for treat,flavor, user, flavortreat

--- PierreSweetandSalty/Data/ApplicationDbContext.cs
+++ PierreSweetandSalty/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PierreSweetandSalty.Models;
 
 namespace PierreSweetandSalty.Data
 {
@@ -14,7 +15,7 @@ namespace PierreSweetandSalty.Data
 
         public virtual DbSet<Flavor> Flavors { get; set; }
         public DbSet<Treat> Treats { get; set; }
-        public DbSet<TreatFlavor> TreatFlavor { get; set; }
+        public DbSet<FlavorTreat> TreatFlavor { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
         }
